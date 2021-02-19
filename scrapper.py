090eb975ef99e99d3502a0977b42fe4bae29b6f3
7GitHub7 <46089991+7GitHub7@users.noreply.github.com>
2021-02-19 17:21:51 +0100

    refactor version first

--- scrapper.py
+++ scrapper.py
@@ -7,29 +7,35 @@ now = datetime.datetime.now()
 maker = sys.argv[1]
 model = sys.argv[2]
 
-path = ''
-fileName = maker + '-' + model + '-' + str(now.date()) + '.csv'
+PATH = 'https://www.otomoto.pl/osobowe/citroen/c5/rawa-mazowiecka/?search%5Bfilter_enum_fuel_type%5D%5B0%5D=petrol&search%5Bfilter_enum_fuel_type%5D%5B1%5D=petrol-lpg&search%5Border%5D=created_at_first%3Adesc&search%5Bbrand_program_id%5D%5B0%5D=&search%5Bdist%5D=155&search%5Bcountry%5D='
+FILE_NAME = maker + '-' + model + '-' + str(now.date()) + '.csv'
+
+
+def get_html_page_as_bs(link):
+    res = requests.get(PATH)
+    res.raise_for_status()
+    return bs4.BeautifulSoup(res.text, features="lxml")
+
+def get_car_list():
+    return currentPage.select('article.offer-item')
 
 
 #prepare the file
-carFile = open(fileName, 'w', newline="")
-outputWriter = csv.writer(carFile)
+# carFile = open(FILE_NAME, 'w', newline="")
+# outputWriter = csv.writer(carFile)
 
-res = requests.get(path)
-res.raise_for_status()
+carSoup = get_html_page_as_bs(PATH)
 
-#check how many pages are there
-carSoup = bs4.BeautifulSoup(res.text, features="lxml")
 lastPage = int(carSoup.select('.page')[-1].text)
 
 for link in carSoup.find_all('a'):
             print(link.get('href'))
 
 for i in range(1):
-    res = requests.get(path + '?page=' + str(i))
+    res = requests.get(PATH + '?page=' + str(i))
     res.raise_for_status()
     currentPage = bs4.BeautifulSoup(res.text, features='lxml')
-    carList = currentPage.select('article.offer-item')
+    carList = get_car_list()
     print("parsing page " + str(i))
     for car in carList:
         #get the interesting data and write to file
@@ -39,8 +45,8 @@ for i in range(1):
         title = car.find('a',class_='offer-title__link').text.strip()
         currentCarData.append(title)
 
-        for link in car.find_all('a'):
-            print(link.get('href'))
+        # get car 
+        PATH = car.find_all('a')[0]   
 
         #Iterate through parameters
         paramList = ["year", "mileage", "engine_capacity", "fuel_type"]
